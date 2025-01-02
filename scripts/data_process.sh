52726cfedec61cf7c636e7e5b97a4d12ba2bd281
xyh-123 <65176920+xyh-123@users.noreply.github.com>
2025-01-02 21:26:19 +0800

    Add files via upload

--- scripts/data_process.sh
+++ scripts/data_process.sh
@@ -4,4 +4,8 @@ python utools/feature_encoder.py --dataset AVIDa_hIL6 --gpu 1
 #For transfer learning, fill the antigen and antibody sequences in the two datasets with the same length
 python utools/feature_encoder.py --dataset CoVAbDab --gpu 1
 
-
+python utools/protein_feature.py --dataset HIV --gpu 1
+python utools/protein_feature.py --dataset SAbDab --gpu 1 --use_cdr
+python utools/protein_feature.py --dataset AVIDa_hIL6 --gpu 1
+#For transfer learning, fill the antigen and antibody sequences in the two datasets with the same length
+python utools/protein_feature.py --dataset CoVAbDab --gpu 1
